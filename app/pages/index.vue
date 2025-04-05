43114675da494d4cb4998bbfd5aeb46f6e5db557
Adrian Jakob <d3xter@d3xter.dev>
2025-04-05 15:40:01 +0200

    wip

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -51,25 +51,47 @@ const onCapture = async () => {
   setTimeout(() => {
     takingPicture.value = false
     finalImage.value = undefined
-    showCountdown.value = false
   }, 5000) // show picture for 5 seconds
 }
 </script>
 
 <template>
-  <div class="relative w-full h-svh bg-black/5">
+  <div class="relative w-full h-svh bg-black/5"  @mousedown="onTriggerCountdown">
     <img 
       v-if="imageData" 
       :src="finalImage ? finalImage : imageData"
       class="w-full h-svh object-cover"
-      @mousedown="onTriggerCountdown"
     />
-    <div v-if="showCountdown" class="flex justify-center items-center h-svh w-full absolute top-0 left-0 bg-black/10">
-      <div class="flex gap-2 items-center">
-        <div class="text-[20rem] font-bold text-black">
-          {{ countdown }}
+    <div v-if="showCountdown || (takingPicture && finalImage == undefined)" class="flex justify-center items-center h-svh w-full absolute top-0 left-0 bg-black/20">
+      <div class="flex gap-2 items-center w-full h-full justify-center">
+        <div class="text-[20rem] font-bold text-white flex items-center justify-center">
+          <transition name="scale">
+            <span v-if="showCountdown" :key="countdown" class="inline-block absolute transform transition-transform duration-300">{{ countdown }}</span>
+          </transition>
+          <span v-if="!showCountdown && finalImage == undefined">
+            <svg xmlns="http://www.w3.org/2000/svg"  width="128" height="128" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-loader-circle-icon lucide-loader-circle animate-spin"><path d="M21 12a9 9 0 1 1-6.219-8.56"/></svg>
+          </span>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.scale-enter-active,
+.scale-leave-active {
+  transition: transform 0.3s ease;
+}
+
+.scale-enter-from,
+.scale-leave-to {
+  transform: scale(0.5);
+  opacity: 0;
+}
+
+.scale-enter-to,
+.scale-leave-from {
+  transform: scale(1);
+  opacity: 1;
+}
+</style>
