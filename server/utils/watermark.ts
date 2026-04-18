2c87001de0d877b58265dc0ce55f40fe60f389f8
Adrian Jakob <d3xter@d3xter.dev>
2026-04-18 16:19:41 +0200

    new watermark

--- server/utils/watermark.ts
+++ server/utils/watermark.ts
@@ -2,7 +2,7 @@ import sharp from 'sharp';
 
 /** Same composite as `api/capture`: logo on southwest with padding. */
 export async function applyLogoWatermark(imageJpeg: Buffer): Promise<Buffer> {
-  const watermark = (await useStorage('assets:assets').getItemRaw('BMD1_Logo.png')) as Buffer;
+  const watermark = (await useStorage('assets:assets').getItemRaw('BMD2_Logo.png')) as Buffer;
   return sharp(imageJpeg)
     .composite([
       {
